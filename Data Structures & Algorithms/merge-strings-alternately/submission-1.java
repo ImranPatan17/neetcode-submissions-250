c32e750231e43284c26385a85b41cb444ff9c19f
ImranPatan17 <120670223+ImranPatan17@users.noreply.github.com>
2026-07-02 23:34:28 +0530

    Add: merge-strings-alternately - submission-1

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        String res = new String();
+        if(word1.length()==word2.length()){
+            for(int i=0;i<word1.length();i++){
+               res+=(word1.charAt(i)+"");
+               res+=(word2.charAt(i)+"");
+            }
+            return res;
+        }
+        else if(word1.length()>word2.length()){
+            for(int i=0;i<word2.length();i++){
+                res+=(word1.charAt(i)+"");
+                res+=(word2.charAt(i)+"");
+            }
+            res+=word1.substring(word2.length());
+            return res;
+        }
+        else{
+            for(int i=0;i<word1.length();i++){
+                res+=word1.charAt(i)+"";
+                res+=word2.charAt(i)+"";
+            }
+            res+=word2.substring(word1.length());
+            return res; 
+        }
+    }
+}
